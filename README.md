2838247fa63118d0eaf0089baf087eac13ee8161
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-05-04 10:22:14 -0700

    create server.js and add api routes, empty CRUD routes and listener

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Note Taker Starter Code
+# Note Taker
+
+<!-- What was your motivation?
+Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+What problem does it solve?
+What did you learn?
+What makes your project stand out? -->
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const uuid = require('./helpers/uuid');
+let listedNotes = require('./db/db.json');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {});
+
+app.post('/api/notes', (req, res) => {});
+
+app.delete("/api/notes/:id", (req, res) => {});
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+})
