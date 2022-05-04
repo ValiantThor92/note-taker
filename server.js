12f6efcd7c722bbf3dc4f83d1d24fea39064f5e5
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-05-04 10:30:54 -0700

    add logic for get all notes request

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const uuid = require('./helpers/uuid');
-let listedNotes = require('./db/db.json');
+let noteList = require('./db/db.json');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) => {});
+//get request for all notes
+app.get('/api/notes', (req, res) => {
+  console.log(`${req.method} get request for all notes received`);
+  res.json(noteList);
+});
 
 app.post('/api/notes', (req, res) => {});
 
