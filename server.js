000fd1ab69405d33759c5723de0ca5f3c27c4fb0
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-05-04 10:34:50 -0700

    add routes for public/index.html and public/notes.html to server.js

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+})
+
 //get request for all notes
 app.get('/api/notes', (req, res) => {
   console.log(`${req.method} get request for all notes received`);
